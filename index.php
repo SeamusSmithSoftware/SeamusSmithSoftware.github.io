b47ecf246295dea94cc642f2ebdc7a0750e858f2
Seamus Smithe <seamus@sfo.me.uk>
2020-09-03 16:45:47 +0100

    rebuild

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset='utf-8'>
+	<meta http-equiv='X-UA-Compatible' content='IE=edge'>
+	<meta name='viewport' content='width=device-width, initial-scale=1'>
+	<title>Seamus Smith Software</title>
+	<!--CSS-->
+	<!--Bootstrap-->
+	<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' integrity='sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u' crossorigin='anonymous'>
+	<!--FontAwesome-->
+	<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css'>
+	<!--Custom CSS-->
+	<link rel='stylesheet' href='css/style.css'>
+</head>
+<body>
+	<!--HEADER-->
+	<header>
+		<div class='continer-fluid'>
+			<div class='jumbotron text-center'>
+				<h1>About</h1>
+				<p>Hi. My name is Seamus. I am an aspiraing software engineer with a basic
+				understanding of various programming languages such as Python, C#, HTML
+				and CSS. I learnt Python whilst doing GCSE Computer Science, and when i
+				went on to do A-Level Computer Science I ended up learning C#. I have
+				taught myself HTML and CSS, and I am currently doing a web development
+				course through Shaw Academy where I can build on my knowledge. I am also
+				planning on learning C++ soon as well.</p>
+			</div>
+		</div>
+	</header>
+	<nav class='navbar navbar-inverse navbar-fixed-top'>
+		<div class='container-fluid'>
+			<header class='navbar-header'>
+				<button type='button' class='navbar-toggle' data-toggle='collapse' data-target='#myNavbar'>
+					<span class='icon-bar'></span>
+					<span class='icon-bar'></span>
+					<span class='icon-bar'></span>
+				</button>
+				<a class='navbar-brand' href='index.php'>SeamusSmithSoftware</a>
+			</header>
+			<div clas='collapse navbar-collapse' id='myNavbar'>
+				<ul class='nav navbar-nav'>
+					<li><a href='index.php'>About</a></li>
+					<li><a href='software.html'>Software</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<!--MAIN-->
+	<main>
+	</main>
+	<!--FOOTER-->
+	<footer class='text-center'>
+		<nav class='nomargin navbar navbar-inverse'>
+			<div class='container-fluid'>
+				<ul class='nav navbar-nav'>
+					<li class='navbar-text'>Copyright &copy; SeamusSmithSoftware</li>
+					<li><a href='https://www.instagram.com/seamussmithsoftware/'><i class="fa fa-instagram"></i></a></li>
+				</ul>
+				<ul class='nav navbar-nav navbar-right'>
+					<li class='navbar-text'>Copyright &copy; SeamusSmithSoftware</li>
+				</ul>
+			</div>
+		</nav>
+	</footer>
+	<!--JAVASCRIPT-->
+	<!--JQuery-->
+	<script src='https://code.jquery.com/jquery-3.1.0.min.js' integrity='sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s=' crossorigin='anonymous'></script>	
+	<!--Bootstrap JS-->
+	<script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js' integrity='sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa' crossorigin='anonymous'></script>	
+	<!--Custom JS-->
+</body>
+</html>
